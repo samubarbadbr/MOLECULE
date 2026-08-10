9dd8e9ea20f2b207dded839a8d06038e41b8d2ca
samubarbadbr <samubarbadbr@gmail.com>
2026-08-10 11:28:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './', // <-- FONDAMENTALE PER FAR TROVARE I FILE COMPILATI
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
